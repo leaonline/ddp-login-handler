23ec1ca9093075fbe9ae9c1451f4150287f902e1
Jan Küster <jkuester@uni-bremen.de>
2019-10-14 17:52:30 +0200

    initial package created

---
+++ ddp-login-handler.js
@@ -0,0 +1,65 @@
+import { Accounts } from 'meteor/accounts-base'
+import { Meteor } from 'meteor/meteor'
+import { HTTP } from 'meteor/http'
+
+/**
+ * Registers a login handler, that allows to authenticate a user by her accessToken, that
+ * she received from a previous OAuth request.
+ * Requires a valid and OAuth endpoint that validates
+ * the accessToken.
+ *
+ * @param name {String} Name of the service, defaults to 'loginWithLea'
+ * @param identityUrl {String} The url which checks the accessToken and returns the user identity or throws an error.
+ */
+const registerOAuthDDPLoginHandler = ({ name = 'loginWithLea', identityUrl }) => {
+  Accounts.accessToken(name, function (options) {
+    // if the service request does not contain an accessToken or
+    // lea as truthy value, we just skip processing instead of throwing
+    if (!options || !options.lea || !options.accessToken) return
+
+    const { accessToken } = options
+
+    let response
+    const requestOptions = {
+      headers: { Accept: 'application/json', 'User-Agent': userAgent, Authorization: `Bearer ${accessToken}` },
+    }
+
+    try {
+      response = HTTP.get(identityUrl, requestOptions)
+    } catch (err) {
+      throw new Error(`Failed to fetch identity from lea. ${err.message}`), { response: err.response }
+    }
+
+    // we make a simple structural response validation
+    const { data } = response
+    if ('string' !== typeof data.id) {
+      throw new Error(`Unacceptable data result. Expected id, got <${data.id}> value.`)
+    }
+    if ('string' !== typeof data.login) {
+      throw new Error(`Unacceptable data result. Expected login, got <${data.login}> value.`)
+    }
+    if ('string' !== typeof data.accessToken) {
+      throw new Error(`Unacceptable data result. Expected accessToken, got <${data.accessToken}> value.`)
+    }
+
+    let userDoc = Meteor.users.findOne({ 'services.lea.id': data.id })
+    if (!userDoc) {
+      const userId = Meteor.users.insert({
+        createdAt: new Date(),
+        services: {
+          lea: {
+            id: data.id,
+            accessToken: accessToken,
+            username: data.login
+          }
+        }
+      })
+
+      userDoc = Meteor.users.findOne(userId)
+    }
+
+    // the loginHandler is expected to
+    // return a { userId } structure
+    return { userId: userDoc._id }
+  })
+}
---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'leaonline:ddp-login-handler',
+  version: '1.0.0',
+  // Brief, one-line summary of the package.
+  summary: 'Authenticate a remote DDP connect using an accessToken and an OAuth service',
+  // URL to the Git repository containing the source code for this package.
+  git: 'git@github.com:leaonline/ddp-login-handler.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.8.1');
+  api.use('ecmascript');
+  api.mainModule('ddp-login-handler.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('leaonline:ddp-login-handler');
+  api.mainModule('ddp-login-handler-tests.js');
+});
