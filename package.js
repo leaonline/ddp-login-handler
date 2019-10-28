83407d54d98f854196e560e787294959d2e78269
jankapunkt <jkuester@uni-bremen.de>
2019-10-28 16:05:26 +0100

    add missing http in api.use list

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'leaonline:ddp-login-handler',
-  version: '1.0.1',
+  version: '1.0.2',
   // Brief, one-line summary of the package.
   summary: 'Authenticate a remote DDP connect using an accessToken and an OAuth service',
   // URL to the Git repository containing the source code for this package.
@@ -12,7 +12,8 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.8.1');
-  api.use('ecmascript');
+  api.use('ecmascript')
+  api.use('http')
   api.mainModule('ddp-login-handler.js');
 });
 
